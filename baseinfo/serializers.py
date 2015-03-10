06a22247cf1023188bdb3da072986af7d72bf930
Andy Y <andy.yap.cal@gmail.com>
2015-03-09 17:47:59 -0700

    Added UserSerializer to return ID and Username in BaseInfo

--- baseinfo/serializers.py
+++ baseinfo/serializers.py
@@ -1,10 +1,20 @@
 from rest_framework import serializers
 from baseinfo.models import BasicInfo, PercentInfo
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username',)
+
 
 class BasicInfoSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
     class Meta:
         model = BasicInfo
-        lookup_field = "user_basic"
+        fields = ('id', 'income', 'fixed_cost', 'investment', 'savings', 'flex_money', 'user')
 
 
 class PercentInfoSerializer(serializers.ModelSerializer):
