79a6d0a10dff4ead14eee28668a530dea28f9f7d
Andy Y <andy.yap.cal@gmail.com>
2015-03-10 23:49:09 -0700

    Got Promises to work for Envelope Options

--- static/app/scripts/controllers/main.js
+++ static/app/scripts/controllers/main.js
@@ -8,15 +8,46 @@
  * Controller of the budgetmeApp
  */
 angular.module('budgetmeApp')
-  .controller('MainCtrl', function ($scope) {
+  .service('ExpenseService', function($http){
+    this.post = function(receiptName, amount, envelope) {
+        var data = {
+            'name': receiptName,
+            'amount': amount,
+            'envelope': envelope
+        };
+
+        $http.post('/api/expense/create_receipt/', data);
+    };
+  })
+  .factory('EnvelopeFactory', function($http){ 
+    return $http.get('/api/expense/list_envelope/');
+    })
+  .controller('MainCtrl', function ($scope, ExpenseService, EnvelopeFactory, $q) {
     $scope.state = '';
+
+    var getEnvelopes = function(){
+    var deferred = $q.defer();  
+    EnvelopeFactory.success(function(data){
+        $scope.envelopeArray = [];
+            for (var i=0; i < data.length; i++) {
+                $scope.envelopeArray.push(data[i].name);
+                deferred.resolve($scope.envelopeArray);
+                }
+        });
+            return deferred.promise;
+    };
+
+    getEnvelopes();
+
     $scope.toggle =  function(){
     	if ($scope.state === ''){
     		$scope.state = 'toggled';
-    		console.log('clicked');
     	}
     	else {
     		$scope.state = '';
 		}
     };
+
+    $scope.expenseSubmit = function(name){console.log(name)};
+
   });
